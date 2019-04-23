16f659e228c6dc3884a46fae30352738198fd6f5
Francisco Schwertner <rockchico@gmail.com>
2019-04-23 12:34:29 -0300

    definindo layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
 
 //export default () => <div>Hello world!</div>
 
 export default () => (
-    <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />  </div>
+    <Layout>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>What do I like to do? Lots of course but definitely enjoy building  websites.</p>
+        <div style={{ color: `purple` }}>
+            <h1>Hello Gatsby!</h1>
+            <p>What a world.</p>
+            <img src="https://source.unsplash.com/random/400x200" alt="" />  
+        </div>
+    </Layout>
 )
 
